34a661fe7c87c1ea9e1031dbfdf8e579c3fb2961
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-12-03 15:51:15 +0100

    feat: add base file html, css and js

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ Abbellire con CSS o Bootstrap
 Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 mostrare le 10 email solo quando solo al termine delle 10 chiamate all’API
 */
+
+const enpoints = `https://flynn.boolean.careers/exercises/api/random/mail`;
+
+axios.get(enpoints)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  })
